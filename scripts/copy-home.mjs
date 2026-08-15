53f525e1d134d71c29e04279e9bc8a1dd8d2e708
Hao <ha>
2026-08-15 16:14:09 +0800

    add

--- scripts/copy-home.mjs
+++ scripts/copy-home.mjs
@@ -9,7 +9,12 @@ fs.mkdirSync(distDir, { recursive: true });
 fs.copyFileSync(path.join(siteDir, "index.html"), path.join(distDir, "index.html"));
 fs.copyFileSync(path.join(siteDir, "404.html"), path.join(distDir, "404.html"));
 fs.copyFileSync(path.join(siteDir, "aobi.png"), path.join(distDir, "aobi.png"));
+fs.copyFileSync(path.join(siteDir, "aobick.png"), path.join(distDir, "aobick.png"));
 fs.copyFileSync(path.join(siteDir, "aobibg.jpg"), path.join(distDir, "aobibg.jpg"));
+fs.copyFileSync(path.join(siteDir, "aobi_button.svg"), path.join(distDir, "aobi_button.svg"));
+fs.copyFileSync(path.join(siteDir, "aobi_close.svg"), path.join(distDir, "aobi_close.svg"));
+fs.copyFileSync(path.join(siteDir, "aobi_fixed_nobtn.svg"), path.join(distDir, "aobi_fixed_nobtn.svg"));
+fs.copyFileSync(path.join(siteDir, "w_aunt.png"), path.join(distDir, "w_aunt.png"));
 fs.writeFileSync(path.join(distDir, ".nojekyll"), "");
 
 const wiki404 = path.join(distDir, "wiki", "404.html");
